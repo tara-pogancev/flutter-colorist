e146104d0b16f6e94f30fc1dab1d8baed4a69e4e
tara-pogancev <tarapogancev@gmail.com>
2025-12-30 12:21:49 +0100

    Renaming git path

--- colorist/lib/src/colorist_extensions.dart
+++ colorist/lib/src/colorist_extensions.dart
@@ -6,4 +6,11 @@ extension ContextThemeX on BuildContext {
   TextTheme get textTheme => theme.textTheme;
 
   ThemeManagerController get themeManager => ThemeManager.of(this);
+
+  /// Returns true if the current theme brightness is dark mode. If the
+  /// brightness is set to system, it checks the actual platform brightness,
+  /// and returns true if that is dark mode.
+  bool get isDarkMode {
+    return themeManager.brightness.dart == Brightness.dark;
+  }
 }
